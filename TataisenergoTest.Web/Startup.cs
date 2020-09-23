1aee233b77bf526846a735589d38bc7e3d5eba78
Arthur Sultanov <as@boolqa.com>
2020-09-23 21:56:31 +0300

    controller to lowercase

--- TataisenergoTest.Web/Startup.cs
+++ TataisenergoTest.Web/Startup.cs
@@ -26,6 +26,7 @@ namespace TataisenergoTest.Web
                 opt.UseSqlServer(Configuration.GetConnectionString("ApplicationContext")));
             services.AddScoped<IMessageService, MessageService>();
             services.AddSwaggerGen();
+            services.AddRouting(options => options.LowercaseUrls = true);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
